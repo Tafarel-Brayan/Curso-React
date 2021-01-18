b836b86cab72f90b42b86948445c4d4f09e95fd6
Tafarel-Brayan <tafarelbrayan@hotmail.com>
2021-01-18 12:46:36 -0400

    Inserção do CSS e Correção do getElementById

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,4 +1,12 @@
+import React from 'react'
 import ReactDom from 'react-dom';
+import './index.css'
 
-const el = document.getElementById("root");
-ReactDom.render('Olá react', el);
+const tag = <strong>Olá React!</strong>
+
+ReactDom.render(
+    <div>
+        { tag }
+    </div>, 
+    document.getElementById('root')
+);
