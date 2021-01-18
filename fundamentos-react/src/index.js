80486aaa81ea7fb0163562b9d81fd6c370e8685b
Tafarel-Brayan <tafarelbrayan@hotmail.com>
2021-01-18 15:26:22 -0400

    Component com Parametro

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,10 +3,14 @@ import ReactDom from 'react-dom';
 import './index.css'
 
 import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
 ReactDom.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Segundo Componente"
+            subtitulo="Muito Legal!" />
     </div>,
     document.getElementById('root')
 );
