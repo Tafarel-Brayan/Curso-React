8a5c844649d6e3decab4ff03dd4d106fe90292b4
Tafarel-Brayan <tafarelbrayan@hotmail.com>
2021-01-26 17:16:13 -0400

    Estilizando

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <h1>Calculadora</h1>
+    <Calculator/>
+
+    {/* <App /> */}
+
   </React.StrictMode>,
   document.getElementById('root')
 );
