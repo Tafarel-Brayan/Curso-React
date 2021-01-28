8480a6050f4b12a520ab69db8bf0f819f75a6697
Tafarel-Brayan <tafarelbrayan@hotmail.com>
2021-01-28 17:21:20 -0400

    Finalizando Curso Rotas

---
+++ navegacao/src/components/layout/Menu.jsx
@@ -0,0 +1,19 @@
+import './Menu.css'
+
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+const Menu = (props) => (
+    <aside className="Menu" >
+        <nav>
+            <ul>
+                <li><Link to="/" >Inicio</Link></li>
+                <li><Link to="/param/1" >Param #1</Link></li>
+                <li><Link to="/param/2" >Param #2</Link></li>
+                <li><Link to="/about">Sobre</Link></li>
+                <li><Link to="/naoExiste">Não Existe</Link></li>
+            </ul>
+        </nav>
+    </aside>
+)
+export default Menu
