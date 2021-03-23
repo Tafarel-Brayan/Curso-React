d2478994d5a4c640594d9b5304f84e383500c6f0
Tafarel-Brayan <tafarelbrayan@hotmail.com>
2021-03-23 18:46:01 -0300

    Cap hooks Finihsed

--- hooks/src/views/examples/UseCustom.jsx
+++ hooks/src/views/examples/UseCustom.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import { useFetch } from '../../hooks/useFectch';
 
 const UseRef = (props) => {
+
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json';
+
+    /** useFectch é meu hook criado */
+    const response = useFetch(url, 'get');
+
+    function showStates(states){
+        return states.map(
+            state => <li key={state.nome} >{state.nome} - {state.sigla}</li>
+        )
+    }
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+
+            <ul>
+                {
+                    response.data ? showStates(response.data) : <li>Lista Vazia</li>
+                }
+            </ul>
         </div>
     )
 }
